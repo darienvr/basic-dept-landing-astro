43b5704cd8c99b891e20f1e60ce2e03c4d0e432c
darienvr <118293353+darienvr@users.noreply.github.com>
2023-08-29 12:55:40 -0500

    astro deploy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://darienvr.github.io',
+    base: '/basic-dept-landing-astro',
+});
